1c38d5b1758cd5febf4b5c865c7edd419d98ed6e
Matteo Tanzi <mtanzi@uwo.ca>
2022-06-30 23:52:31 -0400

    updates to georgiastatescraper

--- Georgia State/GeorgiaStateScraper.py
+++ Georgia State/GeorgiaStateScraper.py
@@ -25,22 +25,33 @@ file_name = 'GeorgiaState_Faculty_guide.csv'
 def scraper_l(law_url, data_frame):
     law_page_content = requests.get(law_url)
     law_page_soup = BeautifulSoup(law_page_content.content, 'html5lib')
-    law_container = law_page_soup.find('div', attrs={'class':'site-container'})
-    law_sub_container = law_container.find('ul', attrs={'class':'mainul'})
-    prof_link_list = law_sub_container.find_all('div', attrs={'class':'esg-entry-media-wrapper'})
-
-    print(prof_link_list)
+    law_container = law_page_soup.find('div', attrs={'class':'pf-content'})
+    links = law_container.find_all('a', attrs={'class':'eg-invisiblebutton'})
+    
+    ## for each link in links, get href as prof_url, then scrape that site
+    for link in links:
+        prof_url = link.get('href')
+        profile_scrape(prof_url, 'Law', data_frame)
 
 # scraper for math, law and comms
 def scraper_mcc(math_url, chem_url, comms_url, data_frame):
     math_page_content = requests.get(math_url)
     math_page_soup = BeautifulSoup(math_page_content.content, 'html5lib')
+    math_container = math_page_soup.find('div', attrs={'class':'pf-content'})
+    math_links = math_container.find_all('a')
+    for link in math_links:
+        math_prof_url = link.get('href')
 
     chem_page_content = requests.get(chem_url)
     chem_page_soup = BeautifulSoup(chem_page_content.content, 'html5lib')
+    chem_container = chem_page_soup.find('div', attrs={'class':'pf-content'})
+    chem_links = chem_container.find_all('a')
+    for link in chem_links:
+        chem_prof_url = link.get('href')
 
     comms_page_content = requests.get(comms_url)
     comms_page_soup = BeautifulSoup(comms_page_content.content, 'html5lib')
+
 
 # scraper for education, history and polisci
 def scraper_ehp(edu_url, history_url, polisci_url, data_frame):
@@ -61,14 +72,18 @@ def scraper_rp(robinson_url, policy_url, data_frame):
     policy_page_content = requests.get(policy_url)
     policy_page_soup = BeautifulSoup(policy_page_content.content, 'html5lib')
 
+# Scrape profile for department, title, email, phone number
+def profile_scrape(url, unit, data_frame):
+    what = 0
+
 # Create CSV from dataframe
 def dataframe_to_csv(data_frame, file_name):
     data_frame.to_csv(file_name, index=False)
     print("Data Frame Compiled")
 
 ## Function Calls
-scraper_l(law_url)
-# scraper_mcc(math_url, chem_url, comms_url)
-# scraper_ehp(edu_url, history_url, polisci_url)
-# scraper_rp(robinson_url, policy_url)
-# dataframe_to_csv(data_frame, file_name)
+scraper_l(law_url, data_frame)
+scraper_mcc(math_url, chem_url, comms_url, data_frame)
+scraper_ehp(edu_url, history_url, polisci_url, data_frame)
+scraper_rp(robinson_url, policy_url, data_frame)
+dataframe_to_csv(data_frame, file_name)
