565ec3633c11c553eb052d7c361e9c6b18aeca96
Matteo Tanzi <mtanzi@uwo.ca>
2022-08-02 18:43:24 -0400

    Rice updates

--- Rice/rice_scraper.py
+++ Rice/rice_scraper.py
@@ -10,3 +10,12 @@ file_name = 'Rice_Faculty_guide.csv'
 
 url = 'https://ga.rice.edu/administration-faculty/faculty/'
 
+
+# Scraper Algorithm
+page_content = requests.get(url)
+page_soup = BeautifulSoup(page_content.content, 'html5lib')
+
+container = page_soup.find('div', attrs={'id':'textcontainer', 'class':'page_content'})
+text_containers = container.find_all('p')
+
+
